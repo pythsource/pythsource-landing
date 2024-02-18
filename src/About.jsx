5e9e35ee74af0e4134407b8c78f5b4e0912c7f6c
IDEXV <39655269+IDEXV@users.noreply.github.com>
2024-02-18 21:54:37 +0300

    another sign test

--- src/About.jsx
+++ src/About.jsx
@@ -1,10 +1,10 @@
 export default function About() {
-    return (
-        <>
-            <article>
-                <h1 className="text-4xl font-bold">PythSource</h1>
-                <p>A small team.</p>
-            </article>
-        </>
-    )
+  return (
+    <>
+      <article>
+        <h1 className="text-4xl font-bold">PythSource</h1>
+        <p>A small team.</p>
+      </article>
+    </>
+  )
 }
