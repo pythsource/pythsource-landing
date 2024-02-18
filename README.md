788cd2b34d590a1d0b884cec457829e4f690741a
IDEXV <39655269+IDEXV@users.noreply.github.com>
2024-02-18 21:54:37 +0300

    initialize repo

---
+++ README.md
@@ -0,0 +1,2 @@
+# PythSource Landing Page
+Uses [Vite.js](https://vitejs.dev) and [React](https://react.dev).
---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <p>cryo is calling. don't pick up</p>
+    </>
+  )
+}
+
+export default App
