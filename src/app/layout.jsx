3c6b67f32bd1ffa38180e20afeeeeed444e7741c
IDEXV <39655269+IDEXV@users.noreply.github.com>
2024-04-06 20:09:24 +0300

    another minor update

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,5 +1,6 @@
 import './main.scss'
 import '@fontsource-variable/jetbrains-mono'
+import Script from 'next/script'
 
 export const metadata = {
   title: 'PythSource',
@@ -11,6 +12,14 @@ export default function RootLayout({ children }) {
     <html lang="en" className="bg-default text-default" prefix="og: https://ogp.me/ns#">
     <head>
       <link rel="icon" href="/favicon.ico" />
+      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-K8S86SR9M2"/>
+      <Script id="google-analytics">
+        {'  window.dataLayer = window.dataLayer || [];\n' +
+          '  function gtag(){dataLayer.push(arguments);}\n' +
+          '  gtag(\'js\', new Date());\n' +
+          '\n' +
+          '  gtag(\'config\', \'G-K8S86SR9M2\');'}
+      </Script>
     </head>
     <body>
     <div id="root">{children}</div>
