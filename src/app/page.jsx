ec68277431b8695999901cfc3ae65493d3c0c10e
IDEXV <39655269+IDEXV@users.noreply.github.com>
2024-11-04 17:01:40 +0300

    remove forums and blog

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,8 +4,8 @@ export default function RootPage() {
             <center>
                 <img src="/pythsource.png" alt="PythSource Logo" width={125} height={125}></img>
                 <h1>Work in Progress</h1>
-                <h2><a href="https://git.pythsource.com">Git</a> • <a href="https://blog.pythsource.com">Blog</a> • <a href="https://forums.pythsource.com">Forums</a> • <a href="https://status.pythsource.com">Status</a> • <a href="https://discord.gg/fvuHZgwTPp">Discord</a></h2>
-                <h2><a href="https://steamcommunity.com/sharedfiles/filedetails/?id=3243498995">Bound by Fate Campaign (Arma 3)</a> • <a href="https://dpn.pythsource.com">Daily Pulse News (DP1)</a> • <a href="https://dpw.pythsource.com">Data Point Project Wiki</a></h2>
+                <h2><a href="https://git.pythsource.com">Git</a> • <a href="https://status.pythsource.com">Status</a> • <a href="https://discord.gg/fvuHZgwTPp">Discord</a></h2>
+                <h2><a href="https://steamcommunity.com/sharedfiles/filedetails/?id=3243498995">Bound by Fate Campaign (Arma 3)</a> • <a href="https://dpn.pythsource.com">Daily Pulse News</a> • <a href="https://dpw.pythsource.com">Data Point Project Wiki</a></h2>
                 <h2><a href="mailto:contact@pythsource.com">contact@pythsource.com</a></h2>
             </center>
         </>
