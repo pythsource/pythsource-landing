117cd1c81afaa827092d9f89d952c2c2bcad440b
IDEXV <39655269+IDEXV@users.noreply.github.com>
2024-11-05 20:04:59 +0300

    update discord link

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,7 +4,7 @@ export default function RootPage() {
             <center>
                 <img src="/pythsource.png" alt="PythSource Logo" width={125} height={125}></img>
                 <h1>Work in Progress</h1>
-                <h2><a href="https://git.pythsource.com">Git</a> • <a href="https://discord.gg/fvuHZgwTPp">Discord</a> • <a href="https://www.youtube.com/@PythSource">YouTube</a></h2>
+                <h2><a href="https://git.pythsource.com">Git</a> • <a href="https://discord.gg/XBKYEQksdX">Discord</a> • <a href="https://www.youtube.com/@PythSource">YouTube</a></h2>
                 <h2><a href="https://steamcommunity.com/sharedfiles/filedetails/?id=3243498995">Bound by Fate Campaign (Arma 3)</a> • <a href="https://dpn.pythsource.com">Daily Pulse News</a> • <a href="https://dpw.pythsource.com">Data Point Project Wiki</a></h2>
                 <h2><a href="mailto:contact@pythsource.com">contact@pythsource.com</a></h2>
             </center>
