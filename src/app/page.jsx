dbebbb82349e5cdc7cf8fbc2707f58570e0fd681
IDEXV <39655269+IDEXV@users.noreply.github.com>
2025-01-01 21:36:53 +0300

    add statuspage link

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,12 @@
+import Image from "next/image";
+
 export default function RootPage() {
     return (
         <>
             <center>
-                <img src="/pythsource.png" alt="PythSource Logo" width={125} height={125}></img>
+                <Image src="/pythsource.png" alt="PythSource Logo" width={125} height={125} />
                 <h1>Work in Progress</h1>
-                <h2><a href="https://git.pythsource.com">Git</a> • <a href="https://discord.gg/XBKYEQksdX">Discord</a> • <a href="https://www.youtube.com/@PythSource">YouTube</a></h2>
+                <h2><a href="https://git.pythsource.com">Git</a> • <a href="https://discord.gg/XBKYEQksdX">Discord</a> • <a href="https://www.youtube.com/@PythSource">YouTube</a> • <a href="https://status.pythsource.com">Status</a></h2>
                 <h2><a href="https://steamcommunity.com/sharedfiles/filedetails/?id=3243498995">Bound by Fate Campaign (Arma 3)</a> • <a href="https://dpn.pythsource.com">Daily Pulse News</a> • <a href="https://dpw.pythsource.com">Data Point Project Wiki</a></h2>
                 <h2><a href="mailto:contact@pythsource.com">contact@pythsource.com</a></h2>
             </center>
