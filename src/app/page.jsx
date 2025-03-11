16459ada4d75927a264d971888090186322d7584
IDEXV <39655269+IDEXV@users.noreply.github.com>
2025-03-11 20:43:34 +0300

    remove old links

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -6,8 +6,7 @@ export default function RootPage() {
             <center>
                 <Image src="/pythsource.png" alt="PythSource Logo" width={125} height={125} />
                 <h1>Work in Progress</h1>
-                <h2><a href="https://git.pythsource.com">Git</a> • <a href="https://discord.gg/XBKYEQksdX">Discord</a> • <a href="https://www.youtube.com/@PythSource">YouTube</a> • <a href="https://status.pythsource.com">Status</a></h2>
-                <h2><a href="https://steamcommunity.com/sharedfiles/filedetails/?id=3243498995">Bound by Fate Campaign (Arma 3)</a> • <a href="https://dpn.pythsource.com">Daily Pulse News</a> • <a href="https://dpw.pythsource.com">Data Point Project Wiki</a></h2>
+                <h2><a href="https://git.pythsource.com">Git</a> • <a href="https://discord.gg/XBKYEQksdX">Discord</a> • <a href="https://www.youtube.com/@PythSource">YouTube</a> • <a href="https://status.pythsource.com">Uptime</a></h2>
                 <h2><a href="mailto:contact@pythsource.com">contact@pythsource.com</a></h2>
             </center>
         </>
